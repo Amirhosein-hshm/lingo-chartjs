41d4c7100674f53623334447efa09d98485d289c
amirhosein <projecthshm@gmail.com>
2022-01-15 20:57:51 +0330

    🎨  final

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,100 @@
-import { useLayoutEffect, useState } from "react";
-import "./App.css";
+import {
+  ArcElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import React from "react";
+import { Line, Pie } from "react-chartjs-2";
+import styled from "styled-components";
 
-const App = () => {
-  const [graph, setGraph] = useState<any>({
-    labels: [],
-    data: [],
-  });
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-  const graphData = [
-    {
-      label: "React",
-      value: 12,
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
     },
-    {
-      label: "Vue",
-      value: 19,
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
     },
-    {
-      label: "Angular",
-      value: 3,
-    },
-  ];
-
-  useLayoutEffect(() => {
-    const labels: any[] = [];
-    const data: any[] = [];
-
-    graphData?.map((v: any) => {
-      labels.push(v?.label);
-      data.push(v?.value);
-    });
-
-    console.log();
-
-    setGraph({
-      labels: labels,
-      data: data,
-    });
-  }, []);
-
-  const data = {
-    labels: graph.labels,
-    datasets: [
-      {
-        label: "# of Votes",
-        data: graph.data,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  return (
-    <>
-      <div className="App">
-        <h3>React Pie Chart Example - CodeCheef </h3>
-        {/* <div style={{ height: "300px", width: "300px", margin: "0 auto" }}>
-          <Pie data={data} />
-        </div> */}
-      </div>
-    </>
-  );
+  },
 };
 
+const labels = ["آبان", "آذر", "دی"];
+
+export const dataLine = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [4, 0],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
+
+export const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export function App() {
+  return (
+    <>
+      <PieChart>
+        <Pie style={{ width: "500px" }} data={data} />;
+      </PieChart>
+      <PieChart>
+        <Line data={dataLine} options={options} />
+      </PieChart>
+    </>
+  );
+}
+
 export default App;
+
+const PieChart = styled.div`
+  width: 30rem;
+  height: 30rem;
+`;
